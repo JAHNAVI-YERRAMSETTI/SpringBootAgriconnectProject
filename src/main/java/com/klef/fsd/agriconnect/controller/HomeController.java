0f3c8fee76fc1e7c142c29bc0f2b9c49e69a7224
JAHNAVI-YERRAMSETTI <2300031078@kluniversity.in>
2025-05-12 17:14:21 +0530

    committed V1.0 successfully

---
+++ src/main/java/com/klef/fsd/agriconnect/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.klef.fsd.agriconnect.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@CrossOrigin("*")
+@RestController
+public class HomeController {
+
+	@GetMapping("/")
+	public String home() {
+	    return "AgriConnect backend is running!";
+	}
+
+}
